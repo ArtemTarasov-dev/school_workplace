d92b4edd08b2d22e635e63238635198e7403e6a5
Artem <artem999tarasov@gmail.com>
2024-07-31 08:25:11 +0200

    contine

--- client/src/pages/AllCoursePage.tsx
+++ client/src/pages/AllCoursePage.tsx
@@ -1,19 +1,42 @@
 import React from "react";
 import { Course } from "../types/Course";
 import CourseCard from "../components/CourseCard";
-import { styled } from "styled-components";
-const StyleCourse = styled(Courses)
+import styled from "styled-components";
+
+const CourseListContainer = styled.div`
+  display: flex;
+  justify-content: center; /* Центрирование списка */
+  padding: 20px;
+  border: 10px double blue; /* Рамка */
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 20px auto;
+  width: 80%;
+`;
+
+const CourseList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 100%; /* Ширина списка */
+`;
+
+const CourseListItem = styled.li`
+  margin-bottom: 16px;
+  text-align: left; /* Выравнивание текста по левому краю */
+`;
+
 const AllCoursePage: React.FC<{ courses: Course[] }> = ({ courses }) => {
   return (
-    <div className="course-list">
-      <ul>
+    <CourseListContainer>
+      <CourseList>
         {courses.map((course) => (
-          <li key={course.id}>
+          <CourseListItem key={course.id}>
             <CourseCard {...course} />
-          </li>
+          </CourseListItem>
         ))}
-      </ul>
-    </div>
+      </CourseList>
+    </CourseListContainer>
   );
 };
 
